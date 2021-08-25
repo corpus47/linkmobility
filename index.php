09c0dba714ea8681c7727996a6bf26d4665410e1
cOrpus <csuporbela@gmail.com>
2021-08-25 14:02:00 +0200

    Add default files

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
 
-echo "Linkmobility";
+require_once('config.inc.php');
+require_once('system/application.php');
+
+define('DEBUG',true);
+
+$app = new Application();
+
+$app->run();
 
 ?>
